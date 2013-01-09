4ef85d813ec9ed95c77ec58bdf0ff5aac56ee062
Ben Rexin <salzig+github@gmail.com>
2013-01-09 18:48:50 +0100

    check if we should send

--- sender.erl
+++ sender.erl
@@ -1,18 +1,21 @@
 -module(sender).
 -author("Ben Rexin <benjamin.rexin@haw-hamburg.de>").
 
+-define(SLOTS_PER_FRAME, 20).
+-define(SLOT_LENGTH, 1000/?SLOTS_PER_FRAME).
+
 %%
 %% Include files
 %%
--import(util, [log/2]).
+-import(util, [log/2, timestamp/0, time_till_slot/1]).
 %%
 %% Exported Functions
 %%
 %% gen_fsm requires explicit export of his required functions
--export([init/1, handle_sync_event/4, handle_info/3, code_change/4, terminate/3]).
+-export([init/1, handle_sync_event/4, handle_info/3, code_change/4, terminate/3, handle_event/3]).
 
 -behaviour(gen_fsm).
--record( state, { coordinator, socket, datasource, address, port, message }).
+-record( state, { coordinator, socket, datasource, address, port, message, slot }).
 %%
 %% API Functions
 %%
@@ -23,32 +26,38 @@ init([ Coordinator, Socket, Address, Port ]) ->
   { ok, Datasource } = datasource:start(),
   { ok, slot_received, #state{ coordinator=Coordinator, socket=Socket, datasource=Datasource, address=Address, port=Port }}.
 
+% fetch message from datasource
 slot_received({ slot, Slot }, State) ->
-  % fetch message from datasource
-  gen_server:cast(State#state.datasource, { get_data, self() }),
-  { next_state, message_received, State }.
+  gen_server:cast( State#state.datasource, { get_data, self() }),
+  { next_state, message_received, State#state{ slot=Slot } }.
 
 message_received({ message, Message }, State) ->
   % fetch next slot from coordinator
-  gen_server:cast(State#state.coordinator, { nextSlot, self() }),
+  gen_server:cast(State#state.coordinator, { nextSlot, self() }),
   { next_state, next_slot_received, State#state{ message=Message} }.
 
 next_slot_received({ nextSlot, Slot }, State) ->
   % if slot not passed
   % deliver message, and wait for next frame
   % else ?
-  Socket = State#state.socket,
-  Address = State#state.address,
-  Port = State#state.port,
-  Message = State#state.message,
-  Packet = build_packet(Message, Slot),
-  gen_udp:send(Socket, Address, Port, Packet),
+  case time_till_slot(State#state.slot) > 0 of
+    true ->
+      Socket = State#state.socket,
+      Address = State#state.address,
+      Port = State#state.port,
+      Message = State#state.message,
+      Packet = build_packet(Message, Slot),
+      gen_udp:send(Socket, Address, Port, Packet);
+    false ->
+      ok % missed slot, should not send to prevent collision. Drop message?
+  end,
   { next_state, slot_received, State }.
 
-terminate( StateName, StateData, State) ->
+terminate( _StateName, _StateData, State) ->
 	gen_server:cast(State#state.datasource, stop),
 	gen_udp:close(State#state.socket),
 	ok.
+
 handle_event(stop, StateName, State) ->
   utility:log("Sender wird ausgeschaltet"),
   {stop, normal, State};
@@ -56,17 +65,10 @@ handle_event(stop, StateName, State) ->
 %%
 %% non API Functions
 %%
-build_packet(Message, Slot) ->
-  Timestamp = timestamp(),
-  Data = list_to_binary(Message)
-  % Bit Syntax Expressions (Value:Size/TypeSpecifierList) http://www.erlang.org/doc/reference_manual/expressions.html
-  << Data:24/binary,
-     Slot:8/integer-big,
-     Timestamp:64/integer-big>>.
 
-timestamp() ->
-  {MegaSecs, Secs, MicroSecs} = now(), % would erlang:timestamp() also work? i'm unsure
-  (MegaSecs * math:pow(10,9)) * (Secs * math:pow(10,3)) * (MicroSecs div 1000).
+% Bit Syntax Expressions (Value:Size/TypeSpecifierList) http://www.erlang.org/doc/reference_manual/expressions.html
+build_packet( Message, Slot ) ->
+  Timestamp = timestamp().
 
 log(Message) ->
 	util:log( "Sender.log", Message ).

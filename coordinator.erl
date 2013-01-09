10efeec599a3d5a001557d5d21f3c9fb2a373746
Ben Rexin <salzig+github@gmail.com>
2013-01-09 15:25:11 +0100

    add some task descriptions as todo list

--- coordinator.erl
+++ coordinator.erl
@@ -1,3 +1,9 @@
 -module(coordinator).
 -author("Ben Rexin <benjamin.rexin@haw-hamburg.de>").
 -compile([export_all]).
+
+% tasks
+% - start datasource
+% - start receiver
+% - start sender
+% - manage slots
--- sender.erl
+++ sender.erl
@@ -1,3 +1,8 @@
 -module(sender).
 -author("Ben Rexin <benjamin.rexin@haw-hamburg.de>").
 -compile([export_all]).
+
+% tasks
+% - waiting for slot
+% - waiting for message
+% - deliver message

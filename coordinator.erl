7ea7e096849f973cc1096ee81a4a1c4df8c1f62c
anton <anton@dual-core>
2013-01-09 18:12:45 +0100

    coordinator start

--- coordinator.erl
+++ coordinator.erl
@@ -2,8 +2,20 @@
 -author("Ben Rexin <benjamin.rexin@haw-hamburg.de>").
 -compile([export_all]).
 
+-behaviour(gen_server).
 % tasks
 % - start datasource
 % - start receiver
 % - start sender
 % - manage slots
+
+-define(?SENDPORT,14010).
+
+-record(state, {datasourcePID, receiverPID, senderPID}).
+
+start(RecPort,Station,MulticastIP)->
+	gen_server:start(?MODUL,[RecPort,?SENDPORT,Team,Station,MulticastIP],[]).
+
+init([RecPort,SendPort,Station,MulticastIP])->
+	
+	

7a71d2fe50a63f2258c5b6ff0f31bd8f36c46044
Ben Rexin <salzig+github@gmail.com>
2013-01-09 21:24:03 +0100

    start parsing package

--- coordinator.erl
+++ coordinator.erl
@@ -10,16 +10,18 @@
 % - start sender
 % - manage slots
 
+-export([init/1, handle_sync_event/4, handle_cast/2, handle_info/2, code_change/3, terminate/2, handle_event/3, handle_call/3]).
+
 -define(SENDPORT,14010).
 
--record(state, {datasourcePID, receiverPID, senderPID, sendport, recport}).
+-record(state, {datasourcePID, receiverPID, senderPID, sendport, recport, slots=dict:new()}).
 
 start(RecPort,Station,MulticastIP,LocalIP)->
 	gen_server:start(?MODULE,[RecPort,?SENDPORT,Station,MulticastIP,LocalIP],[]).
 
 init([RecPort,SendPort,Station,MulticastIP,LocalIP])->
 	{ok,DatasourcePID} = datasource:start(),
-	
+
 	%http://erldocs.com/R15B/kernel/gen_udp.html
 	%http://erldocs.com/R15B/kernel/inet.html#setopts/2
 	%http://stackoverflow.com/questions/78826/erlang-multicast
@@ -34,10 +36,10 @@ init([RecPort,SendPort,Station,MulticastIP,LocalIP])->
 											 {multicast_loop, true},
 											 {add_membership,{MulticastIP,LocalIP}}]
 								  ),
-	
+
 	{ok,ReceiverPID} = receiver:start(self(),RecSocket),
 	{ok,SenderPID} = sender:start(self(),SendSocket,MulticastIP,RecPort),
-	
+
 	{ok, #state{datasourcePID=DatasourcePID,
 				receiverPID=ReceiverPID,
 				senderPID=SenderPID,
@@ -49,16 +51,25 @@ handle_cast({datasink, Data},State)->
 
 %TODO: Slotberechnung
 handle_cast({recieved, Timestamp, Packet},State)->
+	{_, StationNumber, _, SlotWish, Timestamp} = parse_packet(Packet),
+	Slot = util:slot_from(Timestamp),
+
 	ok.
 
+parse_packet(Packet) ->
+	<<Station:8/binary,
+    StationNumber:2/binary,
+    Data:14/binary,
+    Slot:8/integer-big,
+    Timestamp:64/integer-big
+  >> = Packet,
+  {Station, StationNumber, Data, Slot, Timestamp }.
 
 terminate(normal,State)->
 	gen_server:cast(State#state.datasourcePID,{stop}),
 	gen_server:cast(State#state.receiverPID,{stop}),
 	gen_fsm:cast(State#state.senderPID,{stop}),
 	ok.
-	
-
 
 log(Message) ->
 	util:log("Coordinator.log",Message).
@@ -72,4 +83,3 @@ handle_info(_Info, State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-	

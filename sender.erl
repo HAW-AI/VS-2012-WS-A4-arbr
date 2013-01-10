c1edcfe54d35cce8e0a994cf86d852b5749006d1
anton <anton@dual-core>
2013-01-10 18:11:07 +0100

    sen

--- sender.erl
+++ sender.erl
@@ -7,36 +7,49 @@
 %%
 %% Include files
 %%
--import(util, [log/2, timestamp/0, time_till_slot/1]).
+-import(util, [timestamp/0, time_till_slot/1]).
 %%
 %% Exported Functions
 %%
 %% gen_fsm requires explicit export of his required functions
 -export([init/1, handle_sync_event/4, handle_info/3, code_change/4, terminate/3, handle_event/3]).
-
+-compile([export_all]).
 -behaviour(gen_fsm).
--record( state, { coordinator, socket, datasource, address, port, message, slot }).
+-record( state, { coordinator, socket, datasource, address, port, message, slot=0 }).
 %%
 %% API Functions
 %%
 start( Coordinator, Socket, Address, Port )->
+  log("Starter: gestartet"),
   gen_fsm:start( ?MODULE, [ Coordinator, Socket, Address, Port ], [] ).
 
 init([ Coordinator, Socket, Address, Port ]) ->
+  log("Sender: init"),
   { ok, Datasource } = datasource:start(),
   { ok, slot_received, #state{ coordinator=Coordinator, socket=Socket, datasource=Datasource, address=Address, port=Port }}.
 
 % fetch message from datasource
 slot_received({ slot, Slot }, State) ->
+  log("Slot received"),
   gen_server:cast( State#state.datasource, { get_next_value, self() }),
-  { next_state, message_received, State#state{ slot=Slot } }.
+  log("Nach get_next_value"),
+  { next_state, message_received, State#state{ slot=Slot } };
+slot_received(Unknown, State) ->
+  log("[UNKNOWN] slot_received received message [~p]",[Unknown]),
+  { next_state, slot_received, State}.
 
 message_received({ message, Message }, State) ->
+  log("message received"),
   % fetch next slot from coordinator
   gen_server:cast(State#state.coordinator, { nextSlot, self() }),
-  { next_state, next_slot_received, State#state{ message=Message} }.
+  log("received Message [~p]", [Message]),
+  { next_state, next_slot_received, State#state{ message=Message} };
+message_received(Unknown, State) ->
+  log("[UNKNOWN] message_received received message [~p]",[Unknown]),
+  { next_state, message_received, State}.
 
 next_slot_received({ nextSlot, Slot }, State) ->
+  log("next slot received"),
   % if slot not passed
   % deliver message, and wait for next frame
   % else ?
@@ -49,13 +62,18 @@ next_slot_received({ nextSlot, Slot }, State) ->
       Port = State#state.port,
       Message = State#state.message,
       Packet = build_packet(Message, Slot),
-      gen_udp:send(Socket, Address, Port, Packet);
+      gen_udp:send(Socket, Address, Port, Packet),
+      log("Nachricht gesendet");
     false ->
       ok % missed slot, should not send to prevent collision. Drop message?
   end,
-  { next_state, slot_received, State }.
+  { next_state, slot_received, State };
+next_slot_received(Unknown, State) ->
+  log("[UNKNOWN] next_slot_received received message [~p]",[Unknown]),
+  { next_state, next_slot_received, State}.
 
 terminate( _StateName, _StateData, State) ->
+	log("TERMINATING!"),
 	gen_server:cast(State#state.datasource, stop),
 	gen_udp:close(State#state.socket),
 	ok.
@@ -70,15 +88,20 @@ handle_event(stop, _StateName, State) ->
 
 % Bit Syntax Expressions (Value:Size/TypeSpecifierList) http://www.erlang.org/doc/reference_manual/expressions.html
 build_packet( Message, Slot ) ->
+  log("build packet"),
   Timestamp = timestamp(),
+  log("Timestamp [~p]", [Timestamp]),
+  log("Message [~p]", [Message]),
   Data = list_to_binary(Message),
   % Bit Syntax Expressions (Value:Size/TypeSpecifierList) http://www.erlang.org/doc/reference_manual/expressions.html
-  << Data:24/binary,
-    Slot:8/integer-big,
-    Timestamp:64/integer-big>>.
+  Packet = << Data:24/binary,Slot:8/integer-big,Timestamp:64/integer-big>>,
+  log("Packet [~p]",[Packet]),
+  Packet.
 
 log(Message) ->
 	util:log( "Sender.log", Message ).
+log(Message, Data) ->
+	util:log( "Sender.log", Message, Data ).
 
 %% gen_fsm API requirements
 state_name( _Event, _From, State ) ->
@@ -87,7 +110,8 @@ state_name( _Event, _From, State ) ->
 handle_sync_event( _Event, _From, StateName, State ) ->
   {reply, ok, StateName, State}.
 
-handle_info( _Info, StateName, State ) ->
+handle_info( Info, StateName, State ) ->
+  log("Info: [~p]",[Info]),
   {noreply, StateName, State}.
 
 code_change( _OldVsn, StateName, State, _Extra ) ->

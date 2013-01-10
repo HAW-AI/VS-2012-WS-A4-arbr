870b197ef331bfbc1cb84f3cb443c94d07bc5a81
anton <anton@dual-core>
2013-01-10 18:10:47 +0100

    rec

--- receiver.erl
+++ receiver.erl
@@ -3,7 +3,7 @@
 %% Description: TODO: Add description to receiver
 -module(receiver).
 -behaviour(gen_server).
--import(util,[log/2,timestamp/0]).
+-import(util,[timestamp/0]).
 %%
 %% Include files
 %%
@@ -11,7 +11,7 @@
 %%
 %% Exported Functions
 %%
-%% callbacks für gen_server
+%% callbacks fï¿½r gen_server
 -export([init/1,
          handle_call/3,
          handle_cast/2,
@@ -27,22 +27,26 @@
 %% API Functions
 %%
 start(CoordinatorPID,Socket) ->
-	gen_server:start(?MODULE,[CoordinatorPID, Socket],[]).
+	log("Receiver start"),
+	gen_server:start({global,rec},?MODULE,[CoordinatorPID, Socket],[]).
 
 init([CoordinatorPID,Socket]) ->
-	gen_udp:controlling_process(Socket, self()),
+	log("Receiver init"),
 	{ok, #state{coordinatorPID=CoordinatorPID, socket=Socket}}.
 
 terminate(normal, State) ->
+	log("TERMINATING!"),
 	gen_udp:close(State#state.socket),
 	log("Receiver wurde beendet").
 
-%%TODO: Slot
-handle_cast({udp, _Socket, _IP, _Port, Packet}, State) ->
+handle_info({udp, _Socket, _IP, _Port, Packet}, State) ->
 	log("Paket angekommen"),
 	Timestamp = util:timestamp(),
 	gen_server:cast(State#state.coordinatorPID,{recieved, Timestamp, Packet}),
 	{noreply, State};
+handle_info(_Info, State) ->
+  log("Unknown Info"),
+  {noreply, State}.
 
 handle_cast(stop, State) ->
   {stop, normal, State};
@@ -58,15 +62,13 @@ handle_cast(Any, State) ->
 
 log(Message) ->
 	util:log("Receiver.log",Message).
-
+log(Message, Data) ->
+	util:log("Receiver.log",Message, Data).
 
 
 %% durch gen_server vorgegeben
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
-
-handle_info(_Info, State) ->
-  {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.

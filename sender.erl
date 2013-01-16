a2ec8203b39a4907311ead31e79cad8a361b4164
Ben Rexin <salzig+github@gmail.com>
2013-01-16 13:33:32 +0100

    remove request for next slot

--- sender.erl
+++ sender.erl
@@ -33,40 +33,27 @@ slot_received({ slot, Slot }, State) ->
   %log("[~p]Slot received: [~p]",[State#state.station, Slot]),
   gen_server:cast( State#state.datasource, { get_next_value, self() }),
   %log("[~p]Nach get_next_value",[State#state.station]),
-  { next_state, message_received, State#state{ slot=Slot } };
+  { next_state, message_received, State#state{ slot=Slot, next_slot=Slot } };
 slot_received(Unknown, State) ->
   %log("[[~p]UNKNOWN] slot_received received message [~p]",[State#state.station,Unknown]),
   { next_state, slot_received, State}.
 
 message_received({ message, Message }, State) ->
   %log("[~p]message received",[State#state.station]),
-  % fetch next slot from coordinator
-  gen_server:cast(State#state.coordinator, { nextSlot, self() }),
   %log("[[~p]received Message [~p]", [State#state.station,Message]),
-  { next_state, next_slot_received, State#state{ message=Message} };
-message_received(Unknown, State) ->
-  %log("[[~p]UNKNOWN] message_received received message [~p]",[State#state.station,Unknown]),
-  { next_state, message_received, State}.
-
-next_slot_received({ nextSlot, Slot }, State) ->
-  %log("[~p]next slot received: [~p]",[State#state.station, Slot]),
-  % if slot not passed
-  % deliver message, and wait for next frame
-  % else ?
   TimeLeft = time_till_slot(State#state.slot)+20,
-  %log("[~p] TimeLeft: [~p]",[State#state.station, TimeLeft]),
   case TimeLeft > 0 of
     true ->
 %      timer:sleep(TimeLeft),
       gen_fsm:send_event_after(TimeLeft, deliver),
-      { next_state, deliver, State#state{next_slot=Slot} };
+      { next_state, deliver, State#state{message=Message} };
     false ->
       %log("[~p]TimeLeft<=0",[State#state.station]),
-      { next_state, slot_received, State }
+      { next_state, slot_received, State#state{message=Message} }
   end;
-next_slot_received(Unknown, State) ->
-  %log("[[~p]UNKNOWN] next_slot_received received message [~p]",[State#state.station,Unknown]),
-  { next_state, next_slot_received, State}.
+message_received(Unknown, State) ->
+  %log("[[~p]UNKNOWN] message_received received message [~p]",[State#state.station,Unknown]),
+  { next_state, message_received, State}.
 
 deliver(deliver, State) ->
       Socket = State#state.socket,
